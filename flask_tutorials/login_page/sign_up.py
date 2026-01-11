6f86df3c59ae060bde1ad6cce49613f65c96592c
maheshpanta333 <pantamahesh99@gmail.com>
2026-01-11 12:36:38 +0545

    sign up page done, now ready for future usage with minimal changes

--- flask_tutorials/login_page/sign_up.py
+++ flask_tutorials/login_page/sign_up.py
@@ -1,4 +1,4 @@
-from flask  import Flask, Response, jsonify,render_template
+from flask  import Flask, Response, jsonify,render_template,url_for,redirect,request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -20,14 +20,14 @@ login_manager.login_view="login"
 class User(UserMixin,db.Model):
     id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(250),unique=True,nullable=False)
-    password=db.Coulmn(db.String(250),nullable=False)
+    password=db.Column(db.String(250),nullable=False)
 
 
 #this creates the dbase
 with app.app_context():
     db.create_all()
 
-@login_manager.userloader
+@login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
@@ -36,7 +36,23 @@ def home():
     return render_template("index.html")
 
 
-#checkin in
+#now for the user sign up and for form we need post as well
+@app.route("/register",methods=["GET","POST"])
+def register_user():
+    if request.method =="POST":
+        username=request.form.get("username")
+        password=request.form.get("password")
+#here we see if there is such username and .first returns the first row
+        if User.query.filter_by(username=username).first():
+            return render_template("index.html",error="Username  taken!")
+        
+        hashed_password=generate_password_hash(password=password,method="pbkdf2:sha256")
+        password=""
+        new_user=User(username=username,password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return render_template("success_page.html")
+
 
 if __name__=="__main__":
     app.run(debug=True)
