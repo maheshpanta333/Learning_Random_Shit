980c5907fac904058e876a46c27dd67db987c39e
maheshpanta333 <pantamahesh99@gmail.com>
2026-01-11 10:26:03 +0545

    changed my mind making sign up first

---
+++ flask_tutorials/login_page/sign_up.py
@@ -0,0 +1,31 @@
+from flask  import Flask, Response, jsonify,render_template
+from flask-sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+#init flask app with the database and everything
+app=Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///db.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
+app.config["SECRET_KEY"]="supersecretkey"
+
+#init the database
+db=SQLAlchemy(app)
+login_manager=LoginManager()
+login_manager.init_app(app)
+login_manager.login_view="login"
+
+
+#now we create user class to store about the user by inheriting from UserMixin and db.Model that will create blue_print of the user
+class User(UserMixin,db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
